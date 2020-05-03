804f292f5c1735736aff300eb1322d2f2a428173
johnniefujita <johnnie.fujita@gmail.com>
2020-05-03 10:33:55 -0300

    animations with gsap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { useEffect } from 'react';
-import './styles/App.scss'
-import Header from './components/Header'
+import gsap from "gsap";
+import './styles/App.scss';
+import Header from './components/Header';
 import Banner from './components/Banner';
 import Cases from './components/Cases';
+import IntroOverlay from './components/IntroOverlay';
 
 function App() {
 
   useEffect(()=> {
     let vh = window.innerHeight * .01;
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-  }, [])
+
+    gsap.to("body", 0, {css: { visibility: "visible"}});
+
+    const timeline = gsap.timeline();
+
+    timeline.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 0.5,
+      skewY: 7,
+      stagger: {
+        amount: 0.3
+      }
+    }).to(".overlay-top", 1.6, {
+      height: 0,
+      ease: "expo.inOut",
+      stagger: 0.4
+    }).to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: "ease.inOut",
+      delay: -0.8,
+      stagger: {
+        amount: 0.4
+      }
+    }).to(".intro-overlay", 0, { css:{display: "none"}})
+    .from(".case-image img", 1.6, {
+      scale: 1.4,
+      ease: "expo.inOut",
+      delay: -2,
+      stagger: {
+        amount: 0.4
+      }
+    });
+
+  }, []);
 
   return (
     <div className="App">
+      <IntroOverlay />
       <Header />
       <Banner />
       <Cases />
