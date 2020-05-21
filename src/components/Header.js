8b22db8152b36222447e0155bc4067401aeef3ef
johnniefujita <johnnie.fujita@gmail.com>
2020-05-21 16:09:39 -0300

    The menu is openning properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, withRouter } from "react-router-dom";
+// import { NavLink, withRouter } from "react-router-dom";
 import {ReactComponent as UpArrow} from "../assets/up-arrow-circle.svg";
 import gsap from 'gsap';
 
@@ -20,10 +20,99 @@ const Header = ({ dimensions}) => {
                 duration: 1,
                 y: dimensions.width <= 654 ? "80vh" : "70vh",
                 ease: "expo.inOut"
+            }).to(".hamburguer-menu span", {
+                duration: .6,
+                delay: -1,
+                scaleX: 0,
+                transformOrigin:'50% 0%',
+                ease: "expo.inOut"
+            }).to("#Path_1", {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 5
+                }
+            }).to("#Path_2", {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 20
+                }
+            }).to("#Line_1", {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 40,
+                    strokeDasharray: 18
+                }
+            }).to("#circle", {
+                duration: 0.6,
+                delay: -0.8,
+                css: {
+                    strokeDashoffset: 0
+                }
+            }).to(".hamburguer-menu-close", {
+                duration: 0.6,
+                delay: -0.8,
+                css: {
+                    display: "block"
+                }
             });
         }
         else {
-            
+            tl.to(".App", {
+                duration: 1,
+                y: 0,
+                ease: "expo.inOut"
+            }).to("#circle", {
+                duration: .6,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: -193,
+                    strokeDasharray: 227
+                }
+            }).to("#Path_1", {
+                duration: .4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 10
+                }
+            }).to("#Path_2", {
+                duration: .6,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 10
+                }
+            }).to("#Line_1", {
+                duration: .4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 40,
+                    strokeDasharray: 40
+                }
+            }).to(".hamburguer-menu span", {
+                duration: 0.6,
+                delay: -0.6,
+                scaleX: 1,
+                transformOrigin: "50% 0%",
+                ease: "expo.inOut"
+            }).to(".hamburguer-menu-close", {
+                css: {
+                    display: "none"
+                }
+            }).to("body", {
+                css: { 
+                    overflow: "auto"
+                }
+            }).to("nav", {
+                css: {
+                    display: "none"
+                }
+            })
         }
     })
 
